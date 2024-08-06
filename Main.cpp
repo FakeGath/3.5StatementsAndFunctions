3b4053a6a1ac8d45a8c08ec289b2b58fd804d7d7
FakeGath <givanalvaradoath@gmail.com>
2024-08-06 17:13:04 -0600

    Adding the result variable to have the result in one variable

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,12 @@ int main(   ){
     //Declaration of static variables
     int firstNumber {3}; //First statement
     int secondNumber{7}; //Second statement
+    int result = firstNumber + secondNumber;
+    
 
     //this process prints the result of the sum of the firstNumber and secondNumber variables.
     std:: cout << " The sum of the number "<< firstNumber << " + " << secondNumber << " is: "
-    << firstNumber + secondNumber << std :: endl;
+    << result << std :: endl;
 
 
 }
