898f7511e73e61702045965b8014917df5a07cd6
FakeGath <givanalvaradoath@gmail.com>
2024-08-06 17:10:58 -0600

    Main program that sums two variables

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+// this library includes iostream wich provides basic commands to the code.
+#include <iostream>
+
+int main(   ){
+
+    //Declaration of static variables
+    int firstNumber {3}; //First statement
+    int secondNumber{7}; //Second statement
+
+    //this process prints the result of the sum of the firstNumber and secondNumber variables.
+    std:: cout << " The sum of the number "<< firstNumber << " + " << secondNumber << " is: "
+    << firstNumber + secondNumber << std :: endl;
+
+
+}
