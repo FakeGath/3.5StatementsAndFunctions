1cd8b257e17c34b44c9a1ae401eba1914431499d
FakeGath <givanalvaradoath@gmail.com>
2024-08-06 17:24:48 -0600

    Adds a function that sums two numbers addNumbers

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,24 @@
 // this library includes iostream wich provides basic commands to the code.
 #include <iostream>
 
+// this function sums two paramerts previously given.
+int addNuumbers ( int first_param, int second_param){
+    int result = first_param + second_param;
+    return result;
+}
+
 int main(   ){
 
     //Declaration of static variables
     int firstNumber {3}; //First statement
     int secondNumber{7}; //Second statement
-    int result = firstNumber + secondNumber;
+    int result = firstNumber + secondNumber; // adds the result and sums the first and second statement
+    int sum = addNuumbers(firstNumber,secondNumber);//calls function addNumbers
     
 
     //this process prints the result of the sum of the firstNumber and secondNumber variables.
     std:: cout << " The sum of the number "<< firstNumber << " + " << secondNumber << " is: "
-    << result << std :: endl;
+    << sum << std :: endl;
 
 
 }
